5cabe067ad78d6aede7ae029fdab3c5f8ac75c55
Luca Palermo <luca.palermo98@gmail.com>
2026-02-13 21:19:41 +0100

    added blueprints and animation blueprints for main characters and enemies

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -6,6 +6,10 @@
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	//NO text macro because this is a Fname not an fstring, fstring converts to wide character, but fname can take a string literal
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 
