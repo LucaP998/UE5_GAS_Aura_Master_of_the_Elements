03c6a78e810cb6e8c31f847321e608119a727ff4
Luca Palermo <luca.palermo98@gmail.com>
2026-03-01 10:36:54 +0100

    set up effect applied delegate

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -3,3 +3,14 @@
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 
+void UAuraAbilitySystemComponent::AbilityActorInfoSet()
+{
+	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
+}
+
+void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec,
+	FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Effect applied"));
+	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Red, TEXT("Effect applied!!"));
+}
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -14,11 +14,13 @@ class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
 	
-private:
-	void InitAbilityActorInfo();
 	
 public:
 	AAuraCharacter();
 	virtual void PossessedBy(AController* NewController) override;
 	virtual void OnRep_PlayerState() override;
+	
+	
+private:
+	virtual void InitAbilityActorInfo() override;
 };
