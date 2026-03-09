5d4486eb876b3dbcf4fbcc6652d6f758401f9fb9
Luca Palermo <luca.palermo98@gmail.com>
2026-03-09 19:46:30 +0100

    added attributes initialization through gameplay effect

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -52,5 +52,6 @@ void AAuraCharacter::InitAbilityActorInfo()
 			AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
 		}
 	}
+	InitializePrimaryAttributes();
 }
 
