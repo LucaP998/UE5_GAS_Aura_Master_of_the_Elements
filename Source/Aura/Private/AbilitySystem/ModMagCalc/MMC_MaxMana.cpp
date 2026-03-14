3cef520911ba06b438110693757697da9fe21666
Luca Palermo <luca.palermo98@gmail.com>
2026-03-14 13:51:55 +0100

    added mod magnitude calculation also for max mana

--- Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
@@ -1,20 +1,20 @@
 // Copyright Luca Palermo
 
 
-#include "AbilitySystem/ModMagCalc/MMC_MaxHealth.h"
+#include "AbilitySystem/ModMagCalc/MMC_MaxMana.h"
 
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "Interaction/CombatInterface.h"
 
-UMMC_MaxHealth::UMMC_MaxHealth()
+UMMC_MaxMana::UMMC_MaxMana()
 {
-	VigorDef.AttributeToCapture = UAuraAttributeSet::GetVigorAttribute();
-	VigorDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
-	VigorDef.bSnapshot = false;
-	RelevantAttributesToCapture.Add(VigorDef);
+	IntDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	IntDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	IntDef.bSnapshot = false;
+	RelevantAttributesToCapture.Add(IntDef);
 }
 
-float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
 {
 	// Gather tags from source and target
 	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
@@ -24,11 +24,11 @@ float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffec
 	EvaluationParameters.SourceTags = SourceTags;
 	EvaluationParameters.TargetTags = TargetTags;
 	
-	float Vigor = 0.f;
-	GetCapturedAttributeMagnitude(VigorDef, Spec, EvaluationParameters, Vigor);
-	Vigor = FMath::Max(Vigor, 0.f);
+	float Intelligence = 0.f;
+	GetCapturedAttributeMagnitude(IntDef, Spec, EvaluationParameters, Intelligence);
+	Intelligence = FMath::Max(Intelligence, 0.f);
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
 	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
 	
-	return 80.f + 2.5f * Vigor + 10.f * PlayerLevel;
+	return 50.f + 2.5f * Intelligence + 15.f * PlayerLevel;
 }
