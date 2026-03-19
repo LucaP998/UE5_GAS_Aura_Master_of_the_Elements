460cffc83d0d68cbd542c8a3a567d10d69014e1c
Luca Palermo <luca.palermo98@gmail.com>
2026-03-19 20:41:40 +0100

    added asset manager

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,24 @@
+// Copyright Luca Palermo
+
+
+#include "AuraAssetManager.h"
+
+#include "AuraGameplayTags.h"
+
+// I had to add AssetManagerClassName=/Script/Aura.AuraAssetManager to DefaultEngine.ini in the config folder
+// under /Script/Engine.Engine for this to be set as the default asset manager
+
+// Tags can be added with a DataTable, defined in the DefaultGameplayTags.ini file, or natively in the AuraGameplayTags struct
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	check(GEngine);
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+	FAuraGameplayTags::InitializeNativeGameplayTags();
+}
