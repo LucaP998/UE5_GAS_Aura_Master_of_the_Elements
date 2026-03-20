0900bbe3bbccbe9a9c011187f985bda983da4808
Luca Palermo <luca.palermo98@gmail.com>
2026-03-20 21:06:37 +0100

    gameplay tags are now native, added data asset with tags descriptions

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,20 @@
+// Copyright Luca Palermo
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("AttributeInfo: Could not find info for AttributeTag [%s] on AttributeInfo [%s]."), *AttributeTag.ToString(), *GetNameSafe(this));
+	}
+	return FAuraAttributeInfo();
+}
