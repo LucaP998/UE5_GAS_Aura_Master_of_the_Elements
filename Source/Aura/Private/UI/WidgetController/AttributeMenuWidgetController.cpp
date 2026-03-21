0589aa9140b82d1b704e3ad1b5b7992a107332ad
Luca Palermo <luca.palermo98@gmail.com>
2026-03-21 20:40:46 +0100

    added attribute menu widget controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Luca Palermo
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
