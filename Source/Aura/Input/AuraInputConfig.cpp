a91096c6f27eee972bab683935fb985605c43422
Luca Palermo <luca.palermo98@gmail.com>
2026-03-29 10:02:29 +0200

    started adding input configuration for abilities

---
+++ Source/Aura/Input/AuraInputConfig.cpp
@@ -0,0 +1,22 @@
+// Copyright Luca Palermo
+
+
+#include "AuraInputConfig.h"
+
+#include "InputAction.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+	for (const FAuraInputAction& Action : AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag == InputTag)
+		{
+			return Action.InputAction;
+		}
+	}
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("UAuraInputConfig: Could not find input action for InputTag [%s] on InputConfig [%s]."), *InputTag.ToString(), *GetNameSafe(this));
+	}
+	return nullptr;
+};
