28b64abe10c611430b056eb81655fd7a121baa7b
Luca Palermo <luca.palermo98@gmail.com>
2026-04-06 17:47:33 +0200

    added projectile gameplay ability

--- Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
@@ -16,4 +16,8 @@ class AURA_API UAuraGameplayAbility : public UGameplayAbility
 public: 
 	UPROPERTY(EditDefaultsOnly, Category="Input")
 	FGameplayTag StartupInputTag;
+	
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
 };
