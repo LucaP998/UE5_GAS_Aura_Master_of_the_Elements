4ec4a245ba4561c98452b9bc4bcdefef6d394e50
Luca Palermo <luca.palermo98@gmail.com>
2026-04-24 20:12:19 +0200

    added character class info data asset

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Luca Palermo
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
